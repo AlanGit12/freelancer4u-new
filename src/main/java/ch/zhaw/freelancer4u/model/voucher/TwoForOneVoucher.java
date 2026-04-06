a98b517c6d861a490131b5ff310e9743bc3a8c7e
AlanGit12 <gradeala@students.zhaw.ch>
2026-04-06 22:28:51 +0200

    added Voucher Classes and Interface Closes #16

---
+++ src/main/java/ch/zhaw/freelancer4u/model/voucher/TwoForOneVoucher.java
@@ -0,0 +1,10 @@
+package ch.zhaw.freelancer4u.model.voucher;
+
+import ch.zhaw.freelancer4u.model.JobType;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TwoForOneVoucher {
+    
+    private JobType jobType;
+}
